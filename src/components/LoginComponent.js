89738f41d1aedec75213a8964e86e587bddc48a7
Sarthak Patil <sarthakpatil00@gmail.com>
2020-11-25 10:59:41 +0530

    RegisterLogin1

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap'; 
 
 class Login extends Component {
     render(){
         return(
-            <h1>login</h1>
+            <React.Fragment>
+                <br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-7 col-md-7">
+                            <div className="card">
+                                <div className="card-header"><b>Login</b></div>
+                                <div className="line"></div>
+                                <div className="card-body">
+                                    <form>
+                                        <div className="form-group text-left">
+                                            <label htmlFor="exampleInputEmail1">Email address</label>
+                                            <input type="email" 
+                                                className="form-control" 
+                                                id="email" 
+                                                aria-describedby="emailHelp" 
+                                                placeholder="Enter email"
+                                            />
+                                        </div>
+                                        <div className="form-group text-left">
+                                            <label htmlFor="exampleInputPassword1">Password</label>
+                                            <input type="password" 
+                                                className="form-control" 
+                                                id="password" 
+                                                placeholder="Password"
+                                            />
+                                        </div>
+                                        <button 
+                                            type="submit" 
+                                            className="btn btn-primary"
+                                        >
+                                            Login
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap'; 
 
 class Register extends Component {
     render(){
         return(
-            <h1>register</h1>
+            <React.Fragment>
+                <br />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-7 col-md-7">
+                            <div className="card">
+                                <div className="card-header"><b>Register</b></div>
+                                <div className="line"></div>
+                                <div className="card-body">
+                                    <form>
+                                        <div className="form-group text-left">
+                                            <label htmlFor="exampleInputEmail1">Email address</label>
+                                            <input type="email" 
+                                            className="form-control" 
+                                            id="email" 
+                                            aria-describedby="emailHelp" 
+                                            placeholder="Enter email"
+                                            />
+                                        </div>
+                                        <div className="form-group text-left">
+                                            <label htmlFor="exampleInputPassword1">Password</label>
+                                            <input type="password" 
+                                                className="form-control" 
+                                                id="password" 
+                                                placeholder="Password"
+                                            />
+                                        </div>
+                                        <div className="form-group text-left">
+                                            <label htmlFor="exampleInputPassword1">Confirm Password</label>
+                                            <input type="password" 
+                                                className="form-control" 
+                                                id="confirmPassword" 
+                                                placeholder="Confirm Password"
+                                            />
+                                        </div>
+                                        <label>Register as A</label>
+                                        <div className="form-check text-left">
+                                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="Student" checked />
+                                            <label class="form-check-label" for="exampleRadios1">
+                                                Student 
+                                            </label>
+                                        </div>
+                                        <div className="form-check text-left">
+                                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="Mentor" />
+                                            <label class="form-check-label" for="exampleRadios2">
+                                                Mentor
+                                            </label>
+                                        </div>
+                                        <br />
+                                        <button 
+                                            type="submit" 
+                                            className="btn btn-primary"
+                                        >
+                                            Register
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
